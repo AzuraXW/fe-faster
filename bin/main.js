6688e3475ef5647b033ec80cf42ffa3b3ec7a54f
wangwei <wangwei@wangweideMacBook-Pro.local>
2023-10-07 10:37:38 +0800

    feat(create): 增加options替代交互式询问

--- bin/main.js
+++ bin/main.js
@@ -1,25 +1,13 @@
 #! /usr/bin/env node
 
 import { program } from "commander";
-import create from "../lib/action/create.js";
+import create from "../lib/cli/create.js";
 import pkg from "../lib/utils/get-pkg.js";
 
 program.name(pkg.name).usage("<command> [options]");
 program.version(`${pkg.name} ${pkg.version}`);
 
 // create命令
-program
-  .command("create")
-  .argument("project-name")
-  .description("create a new project")
-  .option(
-    "-p, --preset <presetName>",
-    "Skip prompts and use saved or remote preset"
-  )
-  .option("-d, --default", "Skip prompts and use default preset", false)
-  .option("-f, --force", "overwrite target directory if it exists")
-  .action((name, cmd) => {
-    create(name, cmd);
-  });
+create(program);
 
 program.parse(process.argv);
